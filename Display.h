9fc94b1560084f9d74c7d09a6f9db61dfd0761f2
Zacharyat19 <Zacharyat19@gmail.com>
2026-04-03 17:35:52 -0700

    Created header file

---
+++ Display.h
@@ -0,0 +1,16 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#define PAYLOAD_SIZE 6
+#define DISPLAY_NUM_0 0xC0
+#define DISPLAY_NUM_1 0xF9
+
+bool process_is_running(const char* process_name);
+int serial_init(const char* device_path);
+void serial_transmit(int file_descriptor, const uint8_t* payload, size_t size);
+
+#endif
